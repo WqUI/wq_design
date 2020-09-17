562fd2711581d9cfa93f82d83f344ee028feaaf3
刘婵 <liuchan@feewee.cn>
2020-09-17 11:23:25 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App" >
+    <div className="App">
       <header className="App-header" >
         <img src={logo} className="App-logo" alt="logo" />
         <p>
